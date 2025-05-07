bd737ece8c652f83100b7188a2e7fd2250124c8b
NIMBO-AG <80452436+NIMBO-AG@users.noreply.github.com>
2025-05-07 21:03:43 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
 const WEBHOOK_URL      = "https://script.google.com/macros/s/AKfycbwFEYW4nGPTRTmE2VGzZkSRRRvP-yc3RKhP4VOQdg9G5qatvwoMEiymVVGqJaZYQQE/exec";
-const QUESTIONS_URL    = WEBHOOK_URL + "?questions=true";
+const QUESTIONS_URL    = WEBHOOK_URL + "?blocks=true";
 const TRANSLATIONS_URL = WEBHOOK_URL + "?translations=true";
