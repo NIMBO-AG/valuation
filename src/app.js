975e2cf65fdcb808ee1aeba5c6905a1b72c6b64a
NIMBO-AG <80452436+NIMBO-AG@users.noreply.github.com>
2025-05-09 10:46:24 +0200

    Delete src directory
    delete to go back to previous version

--- src/app.js
+++
@@ -1,5 +0,0 @@
-// src/app.js
-ReactDOM.render(
-  React.createElement(FormComponent),
-  document.getElementById('nimbo-form')
-);
--- src/uuid.js
+++
@@ -1,8 +0,0 @@
-// src/uuid.js
-function generateUUID() {
-  return crypto.randomUUID ? crypto.randomUUID() :
-    'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
-      const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
-      return v.toString(16);
-    });
-}
